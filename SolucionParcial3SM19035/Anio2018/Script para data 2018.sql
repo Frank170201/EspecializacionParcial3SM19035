761af2801869a2359322b848e71678b870a9d548
Francisco Santamaria <sm19035@ues.edu.sv>
2024-10-25 11:24:47 -0600

    cambios a dimensiones

--- SolucionParcial3SM19035/Anio2018/Script para data 2018.sql
+++ SolucionParcial3SM19035/Anio2018/Script para data 2018.sql
@@ -8,7 +8,7 @@ go
 
 create table DimAirline(
 AirlineKey int identity(1,1) primary key,
-AirlineID varchar(255) not null,
+AirlineID int not null,
 DOT_ID_Marketing_Airline varchar(255) not null,
 IATA_Code_Marketing_Airline varchar(255) not null,
 Marketing_Airline_Network varchar(255) not null,
@@ -18,7 +18,7 @@ IATA_Code_Operating_Airline  varchar(100) not null,
 
 create table DimDelay(
 DelayKey int identity(1,1) primary key,
-WheelsOff varchar(255) not null,
+WheelsOff int not null,
 DepTime varchar(255)
 )
 
@@ -29,7 +29,7 @@ Month tinyint not null,
 Quarter int not null,
 DayOfWeek int not null,
 DayOfMonth int not null,
-FligthDate datetime not null,
+FligthDate int not null,
 )
 
 create table FactFlight(
